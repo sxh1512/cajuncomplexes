3b41290723c92e9af87dbb24b5905078963fd331
Sergio <sxh1512@louisiana.edu>
2018-11-28 19:42:31 -0600

    Fixed table names, allowing them to be edited from the admin portal

--- aptman/models.py
+++ aptman/models.py
@@ -2,20 +2,20 @@ from django.db import models
 
 # Create your models here.
 
-class Apartments(models.Model):
+class Apartment(models.Model):
     apt_id = models.IntegerField(primary_key=True)
     apt_complex = models.CharField(max_length=200)
     monthly_rent = models.DecimalField(decimal_places=2, max_digits=25)
     vacant = models.BooleanField(default=True)
-    tenant = models.CharField(max_length=200, default=None)
+    tenant_id = models.ForeignKey('Tenant', on_delete=models.CASCADE, null=True, blank=True)
     maintenance_needed = models.BooleanField(default=False)
 
     def __str__(self):
         return self.apt_complex + ": " + str(self.apt_id)
 
-class Tenants(models.Model):
+class Tenant(models.Model):
     tenant_id = models.IntegerField(primary_key=True)
-    apt_id = models.ForeignKey(Apartments, on_delete=models.CASCADE)
+    apt_id = models.ForeignKey(Apartment, on_delete=models.CASCADE, null=True, blank=True)
     tenant_name = models.CharField(max_length=200)
     last_payment = models.DateField()
     next_payment = models.DateField()
@@ -26,8 +26,8 @@ class Tenants(models.Model):
         return self.tenant_name
 
 
-class PaymentHistory(models.Model):
-    tenant_id = models.ForeignKey(Tenants, on_delete=models.CASCADE)
+class PastPayment(models.Model):
+    tenant_id = models.ForeignKey(Tenant, on_delete=models.CASCADE)
     payment_date = models.DateField()
     payment_amount = models.DecimalField(decimal_places=2, max_digits=25)
 
