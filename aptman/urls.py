1638204d6b9a7933ad26424f2195f94999e60525
Sergio <sxh1512@louisiana.edu>
2018-11-28 21:50:36 -0600

    Actually added the stylesheets this time

--- aptman/urls.py
+++ aptman/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'aptman'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:apt_id>/', views.details, name='details')
